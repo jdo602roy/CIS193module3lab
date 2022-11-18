c7b5c1174be36c96f8b21e3b3aaaafc01a623613
jdo602roy <roy.dongwoo.cho@gmail.com>
2022-11-17 22:29:48 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+﻿let width = window.prompt("What the width?");
+let height = window.prompt("What the height?");
+let length = window.prompt("What the length?");
+
+console.log("calculated volume is: ", width*height*length);
